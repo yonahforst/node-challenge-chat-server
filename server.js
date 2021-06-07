87f2de314e68fe8cd8fb22ba94c3d9fffae0cd4e
Yonah Forst <yonaforst@hotmail.com>
2021-06-07 10:28:08 +0200

    finished exercise 2

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
 const welcomeMessage = {
   id: 0,
@@ -16,10 +17,89 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
-});
+// app.get("/", function (request, response) {
+//   response.sendFile(__dirname + "/index.html");
+// });
+
+app.get('/messages', function (request, response) {
+  response.send(messages)
+})
+
+// get a single message from the data store (if it exists)
+// otherwise return 404 not found
+app.get('/messages/:id', function (request, response) {
+  // get the message id from the request object
+  const id = request.params.id
+  // find an existing message with that id
+  const message = messages.find(m => m.id == id)
+  // if not found return status 404
+  if (!message) {
+    response.status(404).send()
+    return
+  }
+  // otherwise return the message object
+  response.send(message)
+})
+
+// adds a new message the data store
+app.post('/messages', function (request, response) {
+  // get the message content from the request
+  const message = {
+    from: request.body.from,
+    text: request.body.text,
+  }
+
+
+  // if message is invalid, return status 400
+  if (!isValidMessage(message)) {
+    response.status(400).send()
+    return
+  }
+
+  // assign it a new id
+  message.id = getNextId()
+  // add it to the data store array
+  messages.push(message)
+  // return a 201 status code and the message content
+  response.status(201).send(message)
+})
+
+// removes a specific message from the data store
+app.delete('/messages/:id', function (request, response) {
+  // get the id from the request object
+  const id = request.params.id
+  // find the index of the message with the specified ID
+  const index = messages.findIndex(m => m.id == id)
+  // if that message is not found, return 404
+  if (index == -1) {
+    response.status(404).send()
+    return
+  }
+  // otherwise remove the object at that index from the datastore
+  messages.splice(index, 1)
+  // return status 204
+  response.status(204).send()
+})
 
 app.listen(3000, () => {
    console.log("Listening on port 3000")
   });
+
+// doesnt take any params.
+// calculates the next ID in sequence and returns it
+function getNextId() {
+  // get the highest ID in the datastore
+  const lastMessage = messages[messages.length - 1]
+  // add 1 to it and return
+  return lastMessage ? lastMessage.id + 1 : 0
+}
+
+// takes a message object the param
+// if the from or text properties are missing or empty, return false
+// otherwise return true
+function isValidMessage(message) {
+  if (message.text && message.from) {
+    return true
+  }
+  return false
+}
